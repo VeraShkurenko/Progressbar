9365777f98cb8c5aa37c3f09259b6e30165e6078
VeraShkurenko <98968743+VeraShkurenko@users.noreply.github.com>
2025-02-22 20:45:50 +0000

    changes

--- inputWiindow/сpp-input/resource.h
+++ inputWiindow/сpp-input/resource.h
@@ -1,23 +1,21 @@
-//{{NO_DEPENDENCIES}}
+﻿//{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by Modal Dialog.rc
+// Used by сpp-input.rc
 //
 #define IDC_MYICON                      2
-#define IDD_MODALDIALOG_DIALOG          102
+#define IDD_PPINPUT_DIALOG              102
 #define IDS_APP_TITLE                   103
 #define IDD_ABOUTBOX                    103
 #define IDM_ABOUT                       104
 #define IDM_EXIT                        105
-#define IDI_MODALDIALOG                 107
+#define IDI_PPINPUT                     107
 #define IDI_SMALL                       108
-#define IDC_MODALDIALOG                 109
+#define IDC_PPINPUT                     109
 #define IDR_MAINFRAME                   128
 #define IDD_DIALOG1                     129
-#define IDC_LIST1                       1000
-#define IDC_RADIO1                      1001
-#define IDC_RADIO2                      1002
-#define IDC_RADIO3                      1003
-#define IDC_BUTTON1                     1004
+#define IDC_BUTTON1                     1001
+#define IDC_EDIT1                       1004
+#define IDC_EDIT2                       1005
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
---
+++ inputWiindow/сpp-input/сpp-input.cpp
@@ -0,0 +1,48 @@
+﻿#include <windows.h>
+#include <tchar.h>
+#include "resource.h"
+
+BOOL CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
+
+
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPTSTR lpszCmdLine, int nCmdShow)
+{
+    return DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc);
+}
+
+HWND hLogin, hPassword;
+
+BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        hLogin = GetDlgItem(hWnd, IDC_EDIT1);
+        hPassword = GetDlgItem(hWnd, IDC_EDIT2);
+
+        return TRUE;
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDC_BUTTON1)
+        {
+
+            int length = SendMessage(hLogin, WM_GETTEXTLENGTH, 0, 0);
+            TCHAR* buffer = new TCHAR[length + 1];
+            GetWindowText(hLogin, buffer, length + 1);
+            int lengthP = SendMessage(hPassword, WM_GETTEXTLENGTH, 0, 0);
+            TCHAR* bufferP = new TCHAR[lengthP + 1];
+            GetWindowText(hPassword, bufferP, lengthP + 1);
+
+            bool isAdmin = _tcscmp(buffer, _TEXT("admin")) == 0 && _tcscmp(bufferP, _TEXT("admin")) == 0;
+
+            MessageBox(hWnd, isAdmin ? TEXT("Користувач адмін") : TEXT("Користувач не адмін"), TEXT("Результат"), MB_OK);
+            delete[] buffer;
+        }
+        return TRUE;
+    case WM_SHOWWINDOW:
+        return TRUE;
+    case WM_CLOSE:
+        EndDialog(hWnd, 0);
+        return TRUE;
+    }
+    return FALSE;
+}
